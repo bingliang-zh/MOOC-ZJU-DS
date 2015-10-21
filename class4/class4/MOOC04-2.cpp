55404e6a12687212f9315c089b46e90abd5b01e8
秉亮 <bl_indie@qq.com>
2015-10-21 10:15:24 +0800

    Done.

--- class4/class4/MOOC04-2.cpp
+++ class4/class4/MOOC04-2.cpp
@@ -1,3 +1,43 @@
+﻿/*
+04-树5 Root of AVL Tree   (25分)
+An AVL tree is a self-balancing binary search tree. In an AVL 
+tree, the heights of the two child subtrees of any node differ 
+by at most one; if at any time they differ by more than one, 
+rebalancing is done to restore this property. Figures 1-4 
+illustrate the rotation rules.
+
+Figures 1-4 omitted
+
+Now given a sequence of insertions, you are supposed to tell 
+the root of the resulting AVL tree.
+Input Specification:
+
+Each input file contains one test case. For each case, the 
+first line contains a positive integer N (≤20) which is the 
+total number of keys to be inserted. Then N distinct integer 
+keys are given in the next line. All the numbers in a line are 
+separated by a space.
+
+Output Specification:
+
+For each test case, print the root of the resulting AVL tree 
+in one line.
+
+Sample Input 1:
+
+5
+88 70 61 96 120
+Sample Output 1:
+
+70
+Sample Input 2:
+
+7
+88 70 61 96 120 90 65
+Sample Output 2:
+
+88
+*/
 #include <iostream>
 #include <algorithm>
 using namespace std;
@@ -79,9 +119,25 @@ AVLNode * insert(AVLNode * tree, int var) {
 	}
 	else if (var < tree->value) {
 		tree->lson = insert(tree->lson, var);
+		if (2 == getHeight(tree->lson) - getHeight(tree->rson)) {
+			if (var < tree->lson->value) {
+				tree = LLRotate(tree);
+			}
+			else {
+				tree = LRRotate(tree);
+			}
+		}
 	}
 	else if (var > tree->value) {
 		tree->rson = insert(tree->rson, var);
+		if (2 == getHeight(tree->rson) - getHeight(tree->lson)) {
+			if (var > tree->rson->value) {
+				tree = RRRotate(tree);
+			}
+			else {
+				tree = RLRotate(tree);
+			}
+		}
 	}
 	updateHeight(tree);
 	return tree;
